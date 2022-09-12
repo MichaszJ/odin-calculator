4e8d85045703556cd8d354e5484e1541121e3eb9
MichaszJ <mjski@live.ca>
2022-09-11 23:06:10 -0400

    implemented display functionality

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function divideOperator(numA, numB) {
 function operate(numA, numB, operator) {
     return operator(numA, numB);
 }
+let displayValue = '';
+const displayElement = document.querySelector('.display');
+function updateDisplay() {
+    if (displayElement != null) {
+        displayElement.innerText = displayValue;
+    }
+}
+function appendDisplay(symbol) {
+    // adding spaces between operators and numbers
+    if ('0123456789'.includes(displayValue.substring(displayValue.length - 1)) && !'+-*/'.includes(symbol)) {
+        displayValue += symbol;
+    }
+    else {
+        displayValue += ` ${symbol}`;
+    }
+    updateDisplay();
+}
--- index.ts
+++ index.ts
@@ -18,3 +18,23 @@ function divideOperator(numA: number, numB: number): number {
 function operate(numA: number, numB: number, operator: Function): number {
     return operator(numA, numB);
 }
+
+let displayValue = '';
+
+const displayElement = document.querySelector<HTMLParagraphElement>('.display');
+function updateDisplay(): void {
+    if (displayElement != null) {
+        displayElement.innerText = displayValue;
+    }
+}
+
+function appendDisplay(symbol: string): void {
+    // adding spaces between operators and numbers
+    if ('0123456789'.includes(displayValue.substring(displayValue.length - 1)) && !'+-*/'.includes(symbol)) {
+        displayValue += symbol;
+    } else {
+        displayValue += ` ${symbol}`;
+    }
+
+    updateDisplay();
+}
