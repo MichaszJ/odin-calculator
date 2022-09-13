d7bd08d2b5d11b8729cf3652e9835671e8d62e4d
MichaszJ <mjski@live.ca>
2022-09-12 21:44:05 -0400

    implemented basic expression evaluator

--- index.js
+++ index.js
@@ -12,8 +12,19 @@ function multiplyOperator(numA, numB) {
 function divideOperator(numA, numB) {
     return numA / numB;
 }
+function errorDisplay() {
+    displayValue = 'ERR';
+    updateDisplay();
+    displayValue = '';
+}
 function operate(numA, numB, operator) {
-    return operator(numA, numB);
+    if (numB == 0 && operator == divideOperator) {
+        errorDisplay();
+        return 0;
+    }
+    else {
+        return operator(numA, numB);
+    }
 }
 let displayValue = '';
 const displayElement = document.querySelector('.display');
@@ -29,12 +40,18 @@ function updateDisplay() {
 }
 function appendDisplay(symbol) {
     // adding spaces between operators and numbers
-    if ('0123456789'.includes(displayValue.substring(displayValue.length - 1)) && !'+-*/'.includes(symbol)) {
-        displayValue += symbol;
+    if ('0123456789'.includes(symbol) || !'+-*/'.includes(displayValue.substring(displayValue.length - 1))) {
+        if ('0123456789'.includes(displayValue.substring(displayValue.length - 1)) && !'+-*/'.includes(symbol)) {
+            displayValue += symbol;
+        }
+        else {
+            displayValue += ` ${symbol}`;
+        }
     }
-    else {
-        displayValue += ` ${symbol}`;
-    }
+    updateDisplay();
+}
+function clearDisplay() {
+    displayValue = '';
     updateDisplay();
 }
 function backspaceDisplay() {
@@ -45,4 +62,41 @@ function backspaceDisplay() {
         displayValue = displayValue.substring(0, displayValue.length - 2);
     }
     updateDisplay();
+}
+function getOperator(operatorSymbol) {
+    if (operatorSymbol == '+') {
+        return addOperator;
+    }
+    else if (operatorSymbol == '-') {
+        return subtractOperator;
+    }
+    else if (operatorSymbol == '*') {
+        return multiplyOperator;
+    }
+    else {
+        return divideOperator;
+    }
+}
+function evaluateDisplay() {
+    // naive evaluation without order of operations
+    let splitArray = displayValue.split(' ');
+    if (splitArray.length >= 3) {
+        let numA = Number(splitArray[0]);
+        let operator = getOperator(splitArray[1]);
+        let numB = Number(splitArray[2]);
+        let result = operate(numA, numB, operator);
+        splitArray[2] = result.toString();
+        splitArray = splitArray.slice(2);
+        while (splitArray.length > 1) {
+            numA = Number(splitArray[0]);
+            operator = getOperator(splitArray[1]);
+            numB = Number(splitArray[2]);
+            result = operate(numA, numB, operator);
+            splitArray[2] = result.toString();
+            splitArray = splitArray.slice(2);
+        }
+        // console.log(operate(numA, numB, operator));
+        displayValue = result.toString();
+        updateDisplay();
+    }
 }
--- index.ts
+++ index.ts
@@ -15,8 +15,19 @@ function divideOperator(numA: number, numB: number): number {
     return numA / numB;
 }
 
+function errorDisplay(): void {
+    displayValue = 'ERR';
+    updateDisplay();
+    displayValue = '';
+}
+
 function operate(numA: number, numB: number, operator: Function): number {
-    return operator(numA, numB);
+    if (numB == 0 && operator == divideOperator) {
+        errorDisplay();
+        return 0;
+    } else {
+        return operator(numA, numB);
+    }
 }
 
 let displayValue = '';
@@ -34,12 +45,19 @@ function updateDisplay(): void {
 
 function appendDisplay(symbol: string): void {
     // adding spaces between operators and numbers
-    if ('0123456789'.includes(displayValue.substring(displayValue.length - 1)) && !'+-*/'.includes(symbol)) {
-        displayValue += symbol;
-    } else {
-        displayValue += ` ${symbol}`;
+    if ('0123456789'.includes(symbol) || !'+-*/'.includes(displayValue.substring(displayValue.length - 1))) {
+        if ('0123456789'.includes(displayValue.substring(displayValue.length - 1)) && !'+-*/'.includes(symbol)) {
+            displayValue += symbol;
+        } else {
+            displayValue += ` ${symbol}`;
+        }
     }
 
+    updateDisplay();
+}
+
+function clearDisplay(): void {
+    displayValue = '';
     updateDisplay();
 }
 
@@ -51,4 +69,45 @@ function backspaceDisplay(): void {
     }
 
     updateDisplay();
+}
+
+function getOperator(operatorSymbol: string) {
+    if (operatorSymbol == '+') {
+        return addOperator;
+    } else if (operatorSymbol == '-') {
+        return subtractOperator;
+    } else if (operatorSymbol == '*') {
+        return multiplyOperator;
+    } else {
+        return divideOperator;
+    }
+}
+
+function evaluateDisplay(): void {
+    // naive evaluation without order of operations
+    let splitArray = displayValue.split(' ');
+
+    if (splitArray.length >= 3) {
+        let numA = Number(splitArray[0]);
+        let operator = getOperator(splitArray[1]);
+        let numB = Number(splitArray[2]);
+        let result = operate(numA, numB, operator);
+
+        splitArray[2] = result.toString();
+        splitArray = splitArray.slice(2);
+
+        while (splitArray.length > 1) {
+            numA = Number(splitArray[0]);
+            operator = getOperator(splitArray[1]);
+            numB = Number(splitArray[2]);
+            result = operate(numA, numB, operator);
+
+            splitArray[2] = result.toString();
+            splitArray = splitArray.slice(2);
+        }
+
+        // console.log(operate(numA, numB, operator));
+        displayValue = result.toString();
+        updateDisplay();
+    }
 }
